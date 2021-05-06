485b6e37f301fdf3493568debfbd360558e56fdf
Cookie's Development <83786645+CookiesDevelopment@users.noreply.github.com>
2021-05-06 15:17:20 -0500

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+-- Manifest
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+name 'FireScript'
+description 'Allows you to simulate fires and smoke.''
+version 'v1.4.0'
+url 'https://github.com/CookiesDevelopment/Firescript'
+client_script 'FireScript.net.dll'
+server_script 'sv_firescript.lua'
